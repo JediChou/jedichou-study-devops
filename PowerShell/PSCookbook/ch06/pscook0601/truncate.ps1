57a82dce10b82677c4eaae585faa5f9c19fd7198
jedichou <jedigit@163.com>
2019-12-19 16:03:35 +0800

    调试目录

---
+++ PowerShell/PSCookbook/ch06/pscook0601/truncate.ps1
@@ -0,0 +1,10 @@
+﻿# File: truncate.ps1
+# From: Windows PowerShell Cookbook, zh-cn version, Page 144
+# Description: Use PowerShell to execute a calculate.
+#
+# PSVersion 5.1.16299.1146
+# PSEdition Desktop
+
+$result = 3/2
+Write-Host $result
+Write-Host ([Math]::Truncate($result))
