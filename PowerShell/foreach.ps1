ea2bffbfd582936987042c1e7a2968c8e3c9275f
jedichou <jedigit@163.com>
2019-12-19 14:27:10 +0800

    增加文件说明

--- PowerShell/foreach.ps1
+++ PowerShell/foreach.ps1
@@ -1,3 +1,7 @@
+# File: foreach.ps1
+# Description: Powershell for each demo
+# Review at 2019.12.19 11:48 by Jedi Chou
+
 Get-ChildItem -path env: | ForEach-Object {
 	Write-Host $_.Name ":" $_.Value;
 }
